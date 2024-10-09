728c78ec4dacd07e8a455212b53169e64ce78ee3
8Israel <137736314+8Israel@users.noreply.github.com>
2024-10-08 20:32:21 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\EncryptionController;
